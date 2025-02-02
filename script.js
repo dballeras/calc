48dbc6634f88ad22d7ba0a767ff0d049610fe2d6
dballeras <151618999+dballeras@users.noreply.github.com>
2025-02-02 18:20:18 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+    //let (inputValue) is the id of the input text
+    let inputValue = document.getElementById("inputValue");
+
+    //You can use any name other than expression for this let
+    let button = "";
+
+    //number bottons&for plus divide times minus
+    function press(num) {
+    button += num;
+    inputValue.value = button;
+    }
+
+    //equal button,used to evaluate the answer inside the inputValue input
+    function equal() {
+    button = eval(button);
+    //inputValue.value = button;
+    inputValue.value = 'I miss you po! 🥺';
+
+    }
+
+    //to remove everything inside the inputValue input
+    function erase() {
+    button = '';
+    inputValue.value = button;
+    }
